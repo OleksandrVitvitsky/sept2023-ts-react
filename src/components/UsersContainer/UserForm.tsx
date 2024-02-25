ab10df328c4b07195bd98ba1e99e879cf10d3a6f
OleksandrVitvitsky <vitvitsky.oleksandr@gmail.com>
2024-02-25 12:36:18 +0200

    task1 create user 1.2 add validators without Joi

--- src/components/UsersContainer/UserForm.tsx
+++ src/components/UsersContainer/UserForm.tsx
@@ -7,7 +7,11 @@ interface IProps{
 }
 const UserForm:FC<IProps> = ({changeTrigger}) => {
 
-    const {reset,register,handleSubmit,setValue} = useForm<IUser>();
+    const {formState:{isValid,errors},reset,register,handleSubmit,setValue} = useForm<IUser>(
+        {
+            mode: 'all'
+        }
+    );
     const save:SubmitHandler<IUser>  = async (user) => {
             await userService.create(user).then(({data}) => console.log(data));
         changeTrigger();
@@ -17,12 +21,60 @@ const UserForm:FC<IProps> = ({changeTrigger}) => {
 
     return (
         <form onSubmit={handleSubmit(save)}>
-            <input type="text" placeholder={"name"} {...register("name")}/>
-            <input type="text" placeholder={"username"} {...register("username")}/>
-            <input type="text" placeholder={"email"} {...register("email")}/>
-            <button>Create</button>
+            <input type="text" placeholder={"name"} {...register("name",{
+                pattern: {
+                    value:/^[a-zA-Zа-яА-яёЁіІїЇ]{1,30}$/,
+                    message: 'min 1 max 30 character'
+                }
+            })}/>
+            <input type="text" placeholder={"username"} {...register("username",{
+                pattern: {
+                    value:/^[a-zA-Zа-яА-яёЁіІїЇ]{1,30}$/,
+                    message: 'min 1 max 30 character'
+                }
+            })}/>
+            <input type="text" placeholder={"email"} {...register("email",{
+                pattern: {
+                    value:/^[a-zA-Zа-яА-яёЁіІїЇ]{1,50}$/,
+                    message: 'min 1 max 50 character'
+                }
+            })}/>
+            <button disabled={!isValid}>Create</button>
+            {errors.name && <div>{errors.name.message}</div>}
+            {errors.username && <div>{errors.username.message}</div>}
+            {errors.email && <div>{errors.email.message}</div>}
         </form>
     );
+
+
+
+    //
+    // return (
+    //     <form onSubmit={handleSubmit(save)}>
+    //         <input type="text" placeholder={'brand'} {...register('brand', {
+    //             pattern: {
+    //                 value: /^[a-zA-Zа-яА-яёЁіІїЇ]{1,20}$/,
+    //                 message: 'min 1 max 2 character'
+    //             }
+    //         })}/>
+    //         <input type="text" placeholder={'price'} {...register('price', {
+    //             valueAsNumber: true,
+    //             min: {value: 0, message: 'min 0'},
+    //             max: {value: 1_000_000, message: 'max 1 000 000'}
+    //         })}/>
+    //         <input type="text" placeholder={'year'} {...register('year', {
+    //             valueAsNumber: true,
+    //             min: {value: 1990, message: 'min 1990'},
+    //             max: {value: new Date().getFullYear(), message: 'max current year'}
+    //         })}/>
+    //         <button disabled={!isValid}>save</button>
+    //         {errors.brand && <div>{errors.brand.message}</div>}
+    //         {errors.price && <div>{errors.price.message}</div>}
+    //         {errors.year && <div>{errors.year.message}</div>}
+    //     </form>
+    // );
+
+
 };
 
 export {UserForm};
